d6897bb63891d7234ff7fee3e04fa017470dae7d
Hilman <arz@Hilmans-MacBook-Pro.local>
2022-04-03 06:01:49 +0700

    change logo

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,3 +1,4 @@
 <a href="/">
-    <img src="https://miro.medium.com/max/1200/1*anSazwoTMKra5IuiJumjng.png" alt="" style="width: 100px; height:100px;">
+    <!-- <img src="https://miro.medium.com/max/1200/1*anSazwoTMKra5IuiJumjng.png" alt="" style="width: 100px; height:100px;"> -->
+    <img src="{{ asset('image/stethoscope.png') }}" alt="" style="width: 100px; height:100px;>
 </a>
